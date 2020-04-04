611912bf7d49e73be34e8c3e453eec1a3e971974
Philipp Müller-Leven <philipp.mueller-leven@gebit.de>
2020-04-04 20:02:25 +0200

    add some variables interference

--- features/annotations/variable.ts
+++ features/annotations/variable.ts
@@ -28,3 +28,22 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x":10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line
+// and initalize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean; // should be = false
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred
