ada3d2a35f93efbc03e0b025dd27ef994931c3a9
Philipp Müller-Leven <philipp.mueller-leven@gebit.de>
2020-04-12 12:47:41 +0200

    Build a CsvReader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,7 @@
-import fs from 'fs';
-import { match } from 'assert';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 // enum - enumeration
 enum MatchResult {
@@ -19,8 +12,8 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === 'Man Unitedd' && match[5] === MatchResult.HomeWin) {
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] == 'Man United' && match[5] == MatchResult.AwayWin) {
     manUnitedWins++;
