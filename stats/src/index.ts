21fa4100d6cfd62ae621294cf1d09527bd9d7d13
Philipp Müller-Leven <philipp.mueller-leven@gebit.de>
2020-04-12 12:38:49 +0200

    Create some football stats app

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello World');
+import fs from 'fs';
+import { match } from 'assert';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man Unitedd' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] == 'Man United' && match[5] == MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+console.log(`Man United won ${manUnitedWins} games`);
