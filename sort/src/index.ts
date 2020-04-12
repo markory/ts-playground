d868d86a53eaa09d9d52b46e1a0c78af2d0cf4f7
Philipp Müller-Leven <philipp.mueller-leven@gebit.de>
2020-04-12 11:52:04 +0200

    Ignore build and .js

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkList';
 
 const numberToSort = new NumbersCollection([10, 7, -5, 0]);
 const sorter = new Sorter(numberToSort);
@@ -11,3 +12,13 @@ const characters = new CharactersCollection('Xaayb');
 const charactersSort = new Sorter(characters);
 charactersSort.sort();
 console.log(charactersSort.collection);
+
+const linkedlist = new LinkedList();
+linkedlist.add(500);
+linkedlist.add(-10);
+linkedlist.add(-3);
+linkedlist.add(4);
+
+const sorterLinkedList = new Sorter(linkedlist);
+sorterLinkedList.sort();
+linkedlist.print();
